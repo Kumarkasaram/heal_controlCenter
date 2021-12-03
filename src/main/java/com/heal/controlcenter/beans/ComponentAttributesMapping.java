d0ff815a021727b4b268f370e358c30782199313
tech_dip <imdip95@gmail.com>
2021-12-03 14:58:03 +0530

    missed file in test case commit

--- src/main/java/com/heal/controlcenter/beans/ComponentAttributesMapping.java
+++ src/main/java/com/heal/controlcenter/beans/ComponentAttributesMapping.java
@@ -2,10 +2,14 @@ package com.heal.controlcenter.beans;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class ComponentAttributesMapping {
 		private int id;
